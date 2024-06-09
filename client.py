4dafd1cd33665b6020df5864f38a27ec6bf61fec
prestonraab <prestonraab@gmail.com>
2024-06-08 23:34:19 -0600

    Hacks

--- client.py
+++ client.py
@@ -14,28 +14,22 @@ if not name:
   st.warning('Please input a name.')
   st.stop()
 
-location = {'latitude': None}
 
-def normal_get_location():
-    global location
+if 'location' not in st.session_state:
     location = streamlit_js_eval.get_geolocation()
     if location:
         st.session_state['iter'] = 1
         st.session_state['location'] = location
         st.rerun()
-
-
-if 'location' not in st.session_state:
-    normal_get_location()
     st.warning('You have not given access to your location.')
     st.stop()
 
+
 def frequent_get_location():
-    global location
     st.session_state['iter'] += 1
     iter = st.session_state['iter']
-    location = streamlit_js_eval.get_geolocation(f"location{iter}")
-    location = streamlit_js_eval.get_geolocation(f"location{iter}")
+    streamlit_js_eval.get_geolocation(f"location{iter}")
+    location = streamlit_js_eval.get_geolocation(f"location{iter - 1}")
     if location:
         st.session_state['location'] = location
     st.write(st.session_state['location'])
