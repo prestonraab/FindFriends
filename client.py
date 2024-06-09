edebb5d5fb98216f5ebe2378bca357bf8488a62c
prestonraab <prestonraab@gmail.com>
2024-06-09 01:55:43 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,35 +3,33 @@ from streamlit_geolocation import streamlit_geolocation
 import js_eval
 
 
-# Function to start watching the location
-def start_watching_location():
-    js_eval.start_watching_location()
-
-
-# Function to retrieve the first location
-def get_first_location():
-    location = js_eval.get_first_location()
-    st.session_state['location'] = location
-
-
-# Function to update the location
-def frequent_get_location():
-    location = js_eval.get_latest_location()
-    if location:
-        st.session_state['location'] = location
-    st.write(st.session_state['location'])
-
-
 if 'watching' not in st.session_state:
-    start_watching_location()
-    st.session_state['watching'] = True
-else:
-    st.write("Watching")
+    sucess = js_eval.start_watching_location()
+    if sucess:
+        st.session_state['watching'] = True
+        st.write("Watch succeeded")
+    else:
+        st.write("Watch failed")
+
+
 
 if 'location' not in st.session_state:
-    get_first_location()
+    location = js_eval.get_first_location()
+    if location:
+        st.session_state['location'] = location
+        st.rerun()
     st.warning('You have not given access to your location.')
     st.stop()
 
-# Call frequent_get_location to update the location
-frequent_get_location()
+
+def frequent_get_location():
+    location = js_eval.get_latest_location()
+    st.write(st.session_state['location'])
+    if location:
+        st.session_state['location'] = location
+    else:
+        st.write(st.session_state['location'])
+
+
+get_location = st.experimental_fragment(frequent_get_location, run_every=1)
+get_location()
