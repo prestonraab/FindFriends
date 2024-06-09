0fb6fd6c153d3dca37cad8bcf8b48f1dd978892a
prestonraab <prestonraab@gmail.com>
2024-06-08 19:40:05 -0600

    Better location

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import geocoder
+from streamlit_geolocation import streamlit_geolocation
 
 st.write("""
 Testing
@@ -7,6 +8,11 @@ Testing
 
 g = geocoder.ip('me')
 st.write(g.latlng)
+
+
+location = streamlit_geolocation()
+
+st.write(location)
 
 name = st.chat_input("What is your name?")
 password = st.chat_input("Enter a word you will remember, or password if you like.")
