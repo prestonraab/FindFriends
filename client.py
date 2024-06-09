d67a9bb36d545dac7bce6c64924b6841749888e7
prestonraab <prestonraab@gmail.com>
2024-06-08 19:18:51 -0600

    Check input and location

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import geocoder
 
 st.write("""
 Testing
 """)
+
+g = geocoder.ip('me')
+print(g.latlng)
+
+name = st.chat_input("What is your name?")
+password = st.chat_input("Enter a word you will remember, or password if you like.")
+
