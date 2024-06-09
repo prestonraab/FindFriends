a7a1aeb61a2e3cad02555d3aaac075548cb97450
prestonraab <prestonraab@gmail.com>
2024-06-09 01:49:04 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,19 @@
-import asyncio
 import streamlit as st
 from streamlit_geolocation import streamlit_geolocation
 import js_eval
 
 # Function to start watching the location
-async def start_watching_location():
-    await js_eval.start_watching_location()
-    st.session_state['watching'] = True
+def start_watching_location():
+    js_eval.start_watching_location()
 
 # Function to retrieve the first location
-async def get_first_location():
-    location = await js_eval.get_first_location()
+def get_first_location():
+    location = js_eval.get_first_location()
     st.session_state['location'] = location
 
 # Function to update the location
-async def frequent_get_location():
-    location = await js_eval.get_latest_location()
+def frequent_get_location():
+    location = js_eval.get_latest_location()
     st.session_state['location'] = location
 
 with st.form("my_form"):
@@ -27,16 +25,17 @@ with st.form("my_form"):
        st.write("name", name)
 
 if 'watching' not in st.session_state:
-    asyncio.run(start_watching_location())
+    start_watching_location()
+    st.session_state['watching'] = True
 
 if not name:
-  st.warning('Please input a name.')
-  st.stop()
+    st.warning('Please input a name.')
+    st.stop()
 
 if 'location' not in st.session_state:
-    asyncio.run(get_first_location())
+    get_first_location()
     st.warning('You have not given access to your location.')
     st.stop()
 
 # Call frequent_get_location to update the location
-asyncio.create_task(frequent_get_location())
+frequent_get_location()
