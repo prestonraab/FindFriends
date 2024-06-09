ebf592aa6e64006513b18314363c41acaf356fed
prestonraab <prestonraab@gmail.com>
2024-06-09 01:32:24 -0600

    Update client.py

--- client.py
+++ client.py
@@ -23,6 +23,8 @@ if not name:
   st.warning('Please input a name.')
   st.stop()
 
+streamlit_geolocation()
+
 if 'location' not in st.session_state:
     location = js_eval.get_first_location()
     if location:
