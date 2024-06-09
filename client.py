1631ef3d38930e34f45c533444a6978680d58737
prestonraab <prestonraab@gmail.com>
2024-06-09 01:53:34 -0600

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,17 @@ import streamlit as st
 from streamlit_geolocation import streamlit_geolocation
 import js_eval
 
+
 # Function to start watching the location
 def start_watching_location():
     js_eval.start_watching_location()
+
 
 # Function to retrieve the first location
 def get_first_location():
     location = js_eval.get_first_location()
     st.session_state['location'] = location
+
 
 # Function to update the location
 def frequent_get_location():
@@ -18,9 +21,12 @@ def frequent_get_location():
         st.session_state['location'] = location
     st.write(st.session_state['location'])
 
+
 if 'watching' not in st.session_state:
     start_watching_location()
     st.session_state['watching'] = True
+else:
+    st.write("Watching")
 
 if 'location' not in st.session_state:
     get_first_location()
