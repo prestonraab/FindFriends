4578e277666800acf259d8cb22565da5768ee3e1
prestonraab <prestonraab@gmail.com>
2024-06-08 23:13:42 -0600

    Looks good

--- client.py
+++ client.py
@@ -19,13 +19,9 @@ location = {'latitude': None}
 def normal_get_location():
     global location
     location = streamlit_js_eval.get_geolocation()
-    # if 'latitude' in location and location['latitude']:
-    #     st.session_state['location'] = location
-    st.write(location)
-    if not location:
-        st.write("Not here")
-    else:
-        st.write(dir(location))
+    if location:
+        st.session_state['location'] = location
+        st.rerun()
 
 
 if 'location' not in st.session_state:
