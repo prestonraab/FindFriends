81a4f52afefacde4455861ba05d4f3958c085c05
prestonraab <prestonraab@gmail.com>
2024-06-08 22:33:51 -0600

    Fancy

--- client.py
+++ client.py
@@ -22,16 +22,16 @@ def normal_get_location():
 
 
 get_location = st.experimental_fragment(normal_get_location)
-get_location()
 
 
 if not location['latitude']:
+  get_location()
   st.warning('You have not given access to your location.')
   st.stop()
+else:
+    location_update_freq = 1
+    location_update = {'run_every' : location_update_freq}
 
-
-location_update_freq = 1
-location_update = {'run_every' : location_update_freq}
-
-get_location = st.experimental_fragment(normal_get_location, run_every=1)
+    get_location = st.experimental_fragment(normal_get_location, run_every=1)
+    get_location()
 
